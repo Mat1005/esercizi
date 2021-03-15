57e52e339650a390135354fdbf71dfa3e413d359
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-03-15 16:11:24 +0100

    Update mostri.py

--- mostri.py
+++ mostri.py
@@ -1,92 +1,10 @@
-'''
-class Entity:
-    def __init___(self, hp, x, y):
-        self.hp = hp
-        self.x = x
-        self.y = y
-    
-    def move(self, direction):
-        if direction == "up":
-            self.y -= 1
-        elif direction == "down":
-            self.y += 1
-        elif direction == "right":
-            self.x += 1
-        elif direction == "left":
-            self.x -=1 
-        
-class Monster(Entity):
-    def __init__(self, x, y, name, damage, field):
-        super().__init__(x, y, field)
-        self.name = name
-        self.hp = 10
-        self.damage = damage
-
-    def info(self):
-        print("Sono",self.name,"faccio",self.damage,"di danno e ho",self.hp,"punti vita su 10; e pi trovo a",self.x,self.y)
-    
-    def attack(self, enemy):
-        if self.hp <= 0:
-            print(self.name, "prova ad attaccare da morto con scarsi risultati")
-        else: 
-            print(self.name, "attacca", enemy.name)
-
-        if (enemy.hp <= 0):
-            print(enemy.name, "e' morto")
-        else:
-            enemy.hp -= self.damage
-
-     
-class Field:
-    def _init_(self):
-        self.h = h
-        self.w = w
-        
-        
-    def draw(self):
-        for y in range(self.h):
-            for x in range(self.w):
-                print("[]", end = "")
-            else:
-                print()
-            
-    
-field1 = Field(10, 10)
-field1.draw()
-
-class Field:
-  def __init__(self):
-    self.w = 5
-    self.h = 5
-    self.entities = []
-
-  def draw(self):
-    for y in range(self.h):
-      for x in range(self.w):
-        for e in self.entities:
-          if x == e.x and y == e.y:
-            print("[x]", end = "")
-            break    
-        else:
-          print("[ ]", end = "")
-      print()
-
-field = Field()
-m = Monster(3, 1, "Pikachu", 10, field)
-m = Monster(1, 3, "Lapras", 10, field)
-field.draw()
-'''
 class Entity: 
   def __init__(self, x, y, field):
     self.x = x
     self.y = y
     self.field = field
     self.field.entities.append(self)
-
-
     
-    
-
 class Monster(Entity):
   def __init__(self, x, y, name, damage, field):
     super().__init__(x, y, field)
@@ -108,16 +26,18 @@ class Monster(Entity):
         else:
             enemy.hp -= self.damage
 
-  def move(self, direction):
-    if direction == "up":
+def move(self, direction):
+    if direction == "up" and self.y >= 1:
       self.y -= 1
-    elif direction == "down":
+    elif direction == "down" and self.y <= 10:
       self.y += 1
-    elif direction == "left":
+    elif direction == "left" and self.x >= 1:
       self.x -= 1
-    elif direction == "right":
+    elif direction == "right" and self.x <= 10:
       self.x += 1
-
+    else:
+        print("Il mostro",self.name,"tenta invano di uscire dall'arena.")
+        
 class Field:
   def __init__(self):
     self.w= 10
