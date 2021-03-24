8c1f4c025d28193d72d3edd6efd76adb68bb0c3b
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-03-24 14:08:32 +0100

    Update mostri.py

--- mostri.py
+++ mostri.py
@@ -11,6 +11,43 @@ class Monster(Entity):
     self.name = name
     self.hp = 10
     self.damage = damage
+  
+  def move(self, direction):
+    casella_vuota = True
+    if direction == "up" and self.y >= 1:
+      for e in field.entities:
+        if self.x == e.x and self.y-1 == e.y:
+          casella_vuota = False
+          print("La casella è occupata")      
+      if casella_vuota:
+        self.y -= 1
+    
+    elif direction == "down" and self.y <= field.h-2:
+      for e in field.entities:
+        if self.x == e.x and self.y+1 == e.y:
+          casella_vuota = False      
+          print("La casella è occupata")
+      if casella_vuota:
+        self.y += 1
+    
+    elif direction == "left" and self.x >= 1:
+      for e in field.entities:
+        if self.x-1 == e.x and self.y == e.y:
+          casella_vuota = False
+          print("La casella è occupata")      
+      if casella_vuota:
+        self.x -= 1
+    
+    elif direction == "right" and self.x <= field.w-2:
+      for e in field.entities:
+        if self.x+1 == e.x and self.y == e.y:
+          casella_vuota = False
+          print("La casella è occupata")      
+      if casella_vuota:
+        self.x += 1
+    
+    else:
+      print("Il mostro",self.name,"tenta invano di uscire dall'arena.")
 
   def info(self):
     print("Sono", self.name, "ho ", self.hp, "/10 hp", "e mi trovo a", self.x, ",", self.y)
@@ -26,18 +63,6 @@ class Monster(Entity):
         else:
             enemy.hp -= self.damage
 
-def move(self, direction):
-    if direction == "up" and self.y >= 1:
-      self.y -= 1
-    elif direction == "down" and self.y <= 10:
-      self.y += 1
-    elif direction == "left" and self.x >= 1:
-      self.x -= 1
-    elif direction == "right" and self.x <= 10:
-      self.x += 1
-    else:
-        print("Il mostro",self.name,"tenta invano di uscire dall'arena.")
-        
 class Field:
   def __init__(self):
     self.w= 10
@@ -60,21 +85,10 @@ m1 = Monster(2, 2, "Bulbasaur", 2, field)
 m2 = Monster(1, 1, "Charmander", 2, field)
 m1.info()
 m2.info()
-field.draw()
-m1.attack(m2)
-m2.info()
+m1.info()
 m1.move("up")
 field.draw()
-m2.attack(m1)
-m1.info()
-m2.move("down")
-m2.move("left")
-field.draw()
-m1.attack(m2)
 m2.info()
-m2.attack(m1)
-m1.info()
-m1.attack(m2)
-m2.info
-m2.attack(m1)
-m1.info()
+m1.move("left")
+field.draw()
+
